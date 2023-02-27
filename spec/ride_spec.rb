1bb380d8550f8008199888aaeb0f4f6298ad2de5
Katie Lonsdale <katie952@gmail.com>
2023-02-27 09:21:11 -0700

    test: add initialize tests

---
+++ spec/ride_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+RSpec.describe Ride do 
+  before(:each) do
+    @ride1 = Ride.new({
+      name: 'Carousel', 
+      min_height: 24, 
+      admission_fee: 1, 
+      excitement: :gentle })
+  end
+
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      expect(@ride1).to be_a Ride
+      expect(@ride1.name).to eq('Carousel')
+      expect(@ride1.min_height).to eq 24
+      expect(@ride1.admission_fee).to eq 1
+      expect(@ride1.excitement).to eq(:gentle)
+    end
+    it 'has a revenue of zero by default' do
+      expect(@ride1.total_revenue).to eq 0
+    end
+
+  end
