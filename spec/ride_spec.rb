7897132f1d204461ad66849f258d81d9967e2b89
Katie Lonsdale <katie952@gmail.com>
2023-02-27 09:53:03 -0700

    test: add test to board rider which prevents riders from boarding if they don't have enough money or aren't tall enough

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -6,9 +6,23 @@ RSpec.describe Ride do
       name: 'Carousel', 
       min_height: 24, 
       admission_fee: 1, 
-      excitement: :gentle })
+      excitement: :gentle 
+    })
+    @ride2 = Ride.new({
+      name: 'Ferris Wheel',
+      min_height: 36,
+      admission_fee: 5,
+      excitement: :gentle 
+    })
+    @ride3 = Ride.new({
+      name: 'Roller Coaster',
+      min_height: 54,
+      admission_fee: 2,
+      excitement: :thrilling 
+    })
     @visitor1 = Visitor.new('Bruce', 54, '$10')
     @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
   end
 
   describe '#initialize' do
@@ -31,6 +45,8 @@ RSpec.describe Ride do
     before(:each) do
       @visitor1.add_preference(:gentle)
       @visitor2.add_preference(:gentle)
+      @visitor2.add_preference(:thrilling)
+      @visitor3.add_preference(:thrilling)
       @ride1.board_rider(@visitor1)
       @ride1.board_rider(@visitor2)
       @ride1.board_rider(@visitor1)
@@ -44,6 +60,17 @@ RSpec.describe Ride do
     end
     it 'adds admission fee to ride total revenue' do
       expect(@ride1.total_revenue).to eq 3
+    end
+    it 'does not board rider unless they meet money and height minimum' do
+      @ride3.board_rider(@visitor1)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
+
+      expect(@visitor1.spending_money).to eq 8
+      expect(@visitor2.spending_money).to eq 4
+      expect(@visitor3.spending_money).to eq 13
+      expect(@ride3.rider_log).to eq({@visitor3 =>1})
+      expect(@ride3.total_revenue).to eq 2
     end
   end
 end
