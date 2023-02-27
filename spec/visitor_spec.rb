705bb230050b827df9144e17699ab7a9a2aa3003
Katie Lonsdale <katie952@gmail.com>
2023-02-27 09:16:05 -0700

    test add tall enough? test

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe do 
   before(:each) do
     @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
   end
 
   describe '#initialize' do
@@ -23,6 +25,15 @@ RSpec.describe do
       @visitor1.add_preference(:water)
 
       expect(@visitor1.preferences).to eq([:gentle, :water])
+    end
+  end
+
+  describe '#tall_enough?' do
+    it 'returns whether visitor is tall enough based on given height' do
+      expect(@visitor1.tall_enough?(54)).to be true
+      expect(@visitor2.tall_enough?(54)).to be false
+      expect(@visitor3.tall_enough?(54)).to be true
+      expect(@visitor1.tall_enough?(64)).to be false
     end
   end
 end
