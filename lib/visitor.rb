6d5c307db8d62930aa1c2af980346fca92267dee
Katie Lonsdale <katie952@gmail.com>
2023-02-27 11:06:17 -0700

    add format spending money helper method

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -5,9 +5,12 @@ class Visitor
   def initialize(name, height, spending_money)
     @name = name
     @height = height
-    @spending_money = spending_money.delete('$').to_i
-    # add helper method
+    @spending_money = format_spending_money(spending_money)
     @preferences = []
+  end
+
+  def format_spending_money(money)
+    @spending_money = money.delete('$').to_i
   end
 
   def add_preference(preference)
